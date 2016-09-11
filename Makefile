73c8af19b36bde2009a768223bbc255e60cb91ef
Joe Frikker <jfrikker@gmail.com>
2016-09-11 11:12:19 -0400

    Adding c++ version

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all
 
-all : primes_hs primes_rs Primes.class
+all : primes_hs primes_rs Primes.class primes_cc
 
 primes_hs : primes.hs
 	ghc -O3 -o primes_hs primes.hs
@@ -10,3 +10,6 @@ primes_rs : primes.rs
 
 Primes.class : Primes.java
 	javac Primes.java
+
+primes_cc : primes.cc
+	g++ -O3 -o primes_cc primes.cc
---
+++ primes.cc
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+
+class Primes {
+  public:
+    Primes();
+    bool isPrime(uint32_t n);
+
+  private:
+    void expand();
+
+    std::vector<uint32_t> primes;
+    uint32_t max;
+};
+
+Primes::Primes() {
+  primes.push_back(2);
+  max = 2;
+}
+
+void Primes::expand() {
+  if (isPrime(max + 1)) {
+    primes.push_back(max + 1);
+  }
+  max++;
+}
+
+bool Primes::isPrime(uint32_t n) {
+  while (max * max < n) {
+    expand();
+  }
+
+  if (max >= n) {
+    return std::binary_search(primes.begin(), primes.end(), n);
+  } else {
+    for (std::vector<uint32_t>::iterator it = primes.begin(); it != primes.end(); it++) {
+      uint32_t i = *it;
+      if (i * i > n) {
+        return true;
+      } else if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+int main(int argc, char** argv) {
+  uint32_t n = atoi(argv[1]);
+  Primes primes;
+  for (uint32_t i = 1; i <= n; i++) {
+    if (primes.isPrime(i)) {
+      std::cout << i << std::endl;
+    }
+  }
+}
